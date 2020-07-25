e85834077146b03920bdbff9ad6e629e30c1d73f
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-07-25 21:24:13 +0900

    update codes

--- HandlingUserInput/StartingPoint/Landmarks/Landmarks/UserData.swift
+++ HandlingUserInput/StartingPoint/Landmarks/Landmarks/UserData.swift
@@ -7,15 +7,9 @@
 //
 
 import SwiftUI
+import Combine
 
-struct UserData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct UserData_Previews: PreviewProvider {
-    static var previews: some View {
-        UserData()
-    }
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
 }
