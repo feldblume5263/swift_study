535973c6f4be5c841a343da6ded0b7550518a850
junhpark <feldblume5263@gmail.com>
2020-08-04 17:02:28 +0900

    update new project

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Junhong Park on 04/08/2020.
+//  Copyright © 2020 Junhong Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
